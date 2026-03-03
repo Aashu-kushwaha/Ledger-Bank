4c9ab4abe0d04993701c3abe0a15f54a7eb8f97e
Aashu-kushwaha <aakushwaha1234567@gmail.com>
2026-03-04 02:35:13 +0530

    ledger service

--- src/app.js
+++ src/app.js
@@ -8,11 +8,14 @@ const transactionRoutes  = require("./routes/transaction.route.js")
 
 const app = express();
 app.use(express.json())//Server cannot read data of body that's why this middlewear help to read the data from body
+app.get("/",(req,res)=>{
+  res.send("Ledger service is up and running.")
+})
+
 app.use(cookieParser());
 app.use("/api/auth",authRouter)
 app.use("/api/accounts",accountRouter)
 app.use("/api/transaction",transactionRoutes)
-
 
 
 module.exports=app
